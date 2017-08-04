2702f8d681a0ff8a823fb0bf26cddc1dd8895f46
Donovan Buck <donovan@donovan.bz>
2017-08-04 00:13:22 -0500

    Some progress

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 'use strict';
 
 const http = require('http');
-const Socket = require('socket.io');
-const send = require('send');
 const url = require('url');
 const REPL = require("repl");
-const ss = require('socket.io-stream');
+const Stream = require('stream');
+
+const Socket = require('socket.io');
+const send = require('send');
 const intercept = require("intercept-stdout");
 const str = require('string-to-stream');
-var stream = require('stream');
 
-const myStream = new stream.Duplex({
-  read(size) {
-    // ...
-  },
-  write(chunk, encoding, callback) {
-    // ...
-  }
-});
 
 function SockMonkey(opts) {
 
@@ -25,6 +17,8 @@ function SockMonkey(opts) {
     return new SockMonkey(opts);
   };
   
+  var inStream = new Stream.Readable();
+
   let app = http.createServer((req, res) => {
     send(req, url.parse(req.url).path, { root: 'public'}).pipe(res);
   });
@@ -40,11 +34,13 @@ function SockMonkey(opts) {
       return "";
     }); 
 
-    const repl = REPL.start({prompt: "> ", terminal: false, input: myStream, output: process.stdout });
+    const repl = REPL.start({prompt: "> ", terminal: false});//, input: inStream, output: process.stdout });
     
     socket.on('monkeySee', function(data) {
-     str(data + "\n").pipe(myStream);
+     str(data + "\n").pipe(process.stdin, { end: false });
     });
+
+    setTimeout(function() { console.log("HIYA")}, 5000 );
     
   });
 
